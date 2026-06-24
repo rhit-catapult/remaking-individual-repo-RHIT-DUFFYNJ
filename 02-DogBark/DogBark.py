a1704da24935741efb54b5f75f335df138d2f875
RHIT-DUFFYNJ <noahduffybuilds@gmail.com>
2026-06-24 14:09:26 -0400

    images, sounds, and text

--- 02-DogBark/DogBark.py
+++ 02-DogBark/DogBark.py
@@ -18,32 +18,42 @@ def main():
 
     # Prepare the image
     # TODO 1: Create an image with the 2dogs.JPG image
+    dogs_image = pygame.image.load("2dogs.JPG")
     # TODO 3: Scale the image to be the size (IMAGE_SIZE, IMAGE_SIZE)
-
+    dogs_image = pygame.transform.scale(dogs_image, (IMAGE_SIZE, IMAGE_SIZE))
     # Prepare the text caption(s)
     # TODO 4: Create a font object with a size 28 font.
+    bottom_font = pygame.font.SysFont("comicsansms", 24)
+    comic_font = pygame.font.SysFont("mistral", 70)
     # TODO 5: Render the text "Two Dogs" using the font object (it's like MAKING an image).
-
-    # Prepare the music
+    bottom_caption = bottom_font.render("Two Dogs", True, BLACK)
+    comic_caption = comic_font.render("FUNNY", True, WHITE)
     # TODO 8: Create a Sound object from the "bark.wav" file.
-
+    bark_sound = pygame.mixer.Sound("bark.wav")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             # TODO 9: Play the music (bark) if there's a mouse click.
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                bark_sound.play()
         # Clear the screen and set the screen background
         screen.fill(WHITE)
 
         # Draw the image onto the screen
         # TODO 2: Draw (blit) the image onto the screen at position (0, 0)
-
+        screen.blit(dogs_image, (0,0))
         # Draw the text onto the screen
         # TODO 6: Draw (blit) the text image onto the screen in the middle bottom.
         # Hint: Commands like these might be useful..
         #          screen.get_width(), caption1.get_width(), image1.get_height()
-
+        capx = screen.get_width() / 2 - bottom_caption.get_width() / 2
+        capy = screen.get_height() - bottom_caption.get_height()
+        screen.blit(bottom_caption, (capx, capy))
+        memex = screen.get_width() / 2 - comic_caption.get_width() / 2
+        memey = screen.get_height() / 2
+        screen.blit(comic_caption, (memex, memey))
         # TODO 7: On your own, create a new bigger font and in white text place a 'funny' message on top of the image.
 
         # Update the screen
