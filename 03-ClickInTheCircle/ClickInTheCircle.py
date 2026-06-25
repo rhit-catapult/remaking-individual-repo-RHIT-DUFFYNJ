6eeef23ce042f603f9ab8038044b8eaa048bc92e
RHIT-DUFFYNJ <noahduffybuilds@gmail.com>
2026-06-25 10:07:29 -0400

    finalizing circle and boilerplate raindrops

--- 03-ClickInTheCircle/ClickInTheCircle.py
+++ 03-ClickInTheCircle/ClickInTheCircle.py
@@ -8,10 +8,7 @@ def distance(point1, point2):
     point1_y = point1[1]
     point2_y = point2[1]
 
-    # TODO 4: Return the actual distance between point 1 and point 2.
-    #  Hint: you will need the math library for the sqrt function.
-    #       distance = sqrt(   (delta x) ** 2 + (delta y) ** 2  )
-    return 0
+    return math.sqrt((point2_x - point1_x)**2 + (point2_y - point1_y)**2)
 
 
 def main():
@@ -21,6 +18,7 @@ def main():
     font = pygame.font.Font(None, 25)
 
     # TODO 8: Load the "drums.wav" file into the pygame music mixer
+    pygame.mixer.music.load("drums.wav")
 
     instruction_text = 'Click in the circle'
     text_color = (222, 222, 0)
@@ -39,22 +37,25 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-            # TODO 2: For a MOUSEBUTTONDOWN event get the click position.
-                # TODO 3: Determine the distance between the click position and the circle_center using the distance
-                # TODO 3:   function and save the result into a variable called distance_from_circle
-                # TODO 5: If distance_from_circle is less than or equal to circle_radius, set message_text to 'Bullseye!'
-                # TODO 5: If distance_from_circle is greater than the circle_radius, set the message_text to 'You missed!'
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if distance(circle_center, pygame.mouse.get_pos()) <= circle_radius:
+                    pygame.mixer.music.play(-1)
+                    message_text = "Hit"
+                else:
+                    message_text = "Miss"
+                    pygame.mixer.music.stop()
+
+
                 # TODO 9: Start playing the music mixer looping forever if the click is within the circle
                 # TODO 10: Stop playing the music if the click is outside the circle
 
         screen.fill(pygame.Color("Black"))
 
-        # TODO 1: Draw the circle using the screen, circle_color, circle_center, circle_radius, and circle_border_width
-
-        # TODO 6: Create a text image (render the text) based on the message_text with the color (122, 237, 201)
+        pygame.draw.circle(screen, circle_color, circle_center, circle_radius, circle_border_width)
+        message_caption = font.render(message_text, True, (122, 237, 201))
+        screen.blit(message_caption, (20, 360))
 
         screen.blit(instructions_image, (25, 25))
-        # TODO 7: Draw (blit) the message to the user that says 'Bullseye!' or 'You missed!'
 
         pygame.display.update()
 
--- 04-Raindrops/RainyDay.py
+++ 04-Raindrops/RainyDay.py
@@ -88,16 +88,25 @@ class Cloud:
 
 def main():
     """ Main game loop that creates the sprite objects, controls interactions, and draw the screen. """
-    # TODO 1: Initialize the game, display a caption, and set   screen   to a 1000x600 Screen.
+    # DONE 1: Initialize the game, display a caption, and set   screen   to a 1000x600 Screen.
+    pygame.init()
+    pygame.display.set_caption("Rainy Day")
+    screen = pygame.display.set_mode((1000, 600))
 
-    # TODO 2: Make a Clock
+    # Done 2: Make a Clock
+    clock = pygame.time.Clock()
     # TODO 7: As a temporary test, make a new Raindrop called test_drop at x=320 y=10
     # TODO 15: Make a Hero, named mike, with appropriate images, starting at position x=200 y=400.
     # TODO 15: Make a Hero, named alyssa, with appropriate images, starting at position x=700 y=400.
     # TODO 23: Make a Cloud, named cloud, with appropriate images, starting at position x=300 y=50.
 
-    # TODO 3: Enter the game loop, with a clock tick of 60 (or so) at each iteration.
-        # TODO 4:   Make the pygame.QUIT event stop the game.
+    # Done 3: Enter the game loop, with a clock tick of 60 (or so) at each iteration.
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+                # Done 4:   Make the pygame.QUIT event stop the game.
 
         # TODO 27: Inside the game loop (AFTER the events loop above), get the list of keys that are currently pressed.
         #     Arrange so that the Cloud moves:
@@ -108,7 +117,8 @@ def main():
         # DISCUSS: If you want something to happen once per key press, put it in the events loop above
         #          If you want something to continually happen while holding the key, put it after the events loop.
 
-        # TODO 5: Inside the game loop, draw the screen (fill with white)
+        # Done 5: Inside the game loop, draw the screen (fill with white)
+        screen.fill(pygame.Color(255,255,255))
 
         # --- begin area of test_drop code that will be removed later
         # TODO 12: As a temporary test, move test_drop
@@ -134,7 +144,7 @@ def main():
         # TODO 18: Draw the Heroes (Mike and Alyssa)
 
         # TODO 6: Update the display and remove the pass statement below
-    pass
+        pygame.display.update()
 
-
-# TODO 0: Call main.
+# Done 0: Call main.
+main()
